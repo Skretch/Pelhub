0f6aba6c17da29f4e2153f65bb2585b4c8f2249d
Anders S <Sortedal3@gmail.com>
2019-03-02 18:46:19 +0100

    added server code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-/*This comment is new :)*/
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.js Server!');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if(err){
+        return console.log('something bad happened', err);
+    }
+
+    console.log(`Server is listening on port: ${port}`);
+})
