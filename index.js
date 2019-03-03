4dd171e87b37122de3ecf3de03c53f3b26bffaa5
Anders S <Sortedal3@gmail.com>
2019-03-03 12:10:12 +0100

    testing webhook

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const requestHandler = (request, response) => {
     console.log(request.url);
-    response.end('Insane!!');
+    response.end('Insane potato!!');
 }
 
 const server = http.createServer(requestHandler);
